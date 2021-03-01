4775a1ccaf7e44193b3c5afbb8859806447e5b66
vavomiskam <vavomiskam@yandex.ru>
2021-03-01 21:15:36 +0300

    start diploma

---
+++ src/main/java/com/diplom/maksimov/MaksimovApplication.java
@@ -0,0 +1,13 @@
+package com.diplom.maksimov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaksimovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaksimovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diplom/maksimov/MaksimovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplom.maksimov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaksimovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
