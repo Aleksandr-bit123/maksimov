f788e4d054b8e21cecffe463ede57bebc68dd032
vavomiskam <vavomiskam@yandex.ru>
2021-03-28 13:02:16 +0300

    + H2 DATABASE

---
+++ src/test/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.t_role (id, name)
+VALUES (1, 'ROLE_USER');
+INSERT INTO public.t_role (id, name)
+VALUES (2, 'ROLE_ADMIN');
+INSERT INTO public.t_role (id, name)
+VALUES (3, 'ROLE_BOSS');
+INSERT INTO public.t_role (id, name)
+VALUES (4, 'ROLE_LOGIST');
+INSERT INTO public.t_role (id, name)
+VALUES (5, 'ROLE_DRIVER');
+INSERT INTO public.t_user (id, password, username)
+VALUES (1000000, null, 'UserForTest');
